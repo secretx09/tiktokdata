f86f0ac354c4cb01b65d665f0ba0851d863d0a35
secretx09 <101412985+secretx09@users.noreply.github.com>
2025-01-13 18:57:59 +0000

    made every option print the text it contains a couple lines down so it's less crowded

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ while True:
     if view == '1': 
         activity_option = int(input("Browsing History(1)\nComments(2)\nFavorite effects, hashtags, sounds, videos(3)\nFollowers(4)\nFollowing(5)\nOther(6)\n>>>"))
         if activity_option == 1:
-            print('browsing history')
-        elif activity_option == 2: 
-            print("comments")
+            print('\n\nbrowsing history')
+        elif activity_option == 2:
+            print("\n\ncomments")
         elif activity_option == 3:
-            print('favorites')
+            print('\n\nfavorites')
         elif activity_option == 4:
-            print('followers')
+            print('\n\nfollowers')
         elif activity_option == 5:
-            print("following")
+            print("\n\nfollowing")
         elif activity_option == 6: 
-            print('other')
+            print('\n\nother')
         else: 
             print("Please choose one of the options (1-6)")
-
 
     #Option 2
     elif view == '2':
@@ -28,9 +27,9 @@ while True:
     elif view == '3':
         settings = int(input('Blocked accounts(1)\nSettings(2)\n>>>'))
         if settings == 1:
-            print("Blocked accounts")
+            print("\n\nBlocked accounts")
         elif settings == 2: 
-            print("Settings")
+            print("\n\nSettings")
         else: 
             print("Please choose one of the 2 options")
     
@@ -46,9 +45,9 @@ while True:
     elif view == '6':
         posts = int(input('Posts(1)\nRecently deleted posts(2)\n>>>'))
         if posts == 1:
-            print("posts")
+            print("\n\nposts")
         elif posts == 2: 
-            print("Recently deleted")
+            print("\n\nRecently deleted")
         else: 
             print("Please choose one of the 2 options")
 
@@ -56,9 +55,9 @@ while True:
     elif view == '7':
         profile = int(input('Profile info(1)\nOther(2)\n>>>'))
         if profile == 1: 
-            print("Profile info")
+            print("\n\nProfile info")
         elif profile == 2: 
-            print("Other")
+            print("\n\nOther")
         else: 
             print("Please choosse one of the 2 options")
 
@@ -66,24 +65,24 @@ while True:
     elif view == '8':
         live = int(input('Live history *comments, watched lives, etc*(1)\nOther(2)\n>>>'))
         if live == 1: 
-            print("live history")
+            print("\n\nlive history")
         elif live == 2: 
-            print("Other")
+            print("\n\nOther")
         else: 
             print("Please choose 1 of the 2 options")
 
     elif view == '9':
         shop = int(input('Orders(1)\nBrowsing History(2)\nShopping Cart(3)\nReviews(4)\nOther(5)\n>>>'))
         if shop == 1: 
-            print('orders')
+            print('\n\norders')
         elif shop == 2: 
-            print("browsing history")
+            print("\n\nbrowsing history")
         elif shop == 3: 
-            print("cart")
+            print("\n\ncart")
         elif shop == 4: 
-            print("reviews")
+            print("\n\nreviews")
         elif shop == 5: 
-            print('other')
+            print('\n\nother')
         else: 
             print("Please choose one of the options (1-5)")
     
