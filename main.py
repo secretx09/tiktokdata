ef766e9eb9027db1cb8e0e52a02a533b22d2af98
secretx09 <101412985+secretx09@users.noreply.github.com>
2025-01-12 20:46:20 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+view = input('What would you like to see? ("list" for list of options) >>> ')
+if view == "list":
+    print("Activity(1)\nAds and data(2)\nApp Settings(3)\nDM's(4)\nIncome + Wallet Transactions(5)\nPosts(6)\nProfile(7)\nTikTok Live(8)\nTikTok Shopping(9)\n")
+    view = input('What would you like to see? ("list" for list of options) >>> ')
+
+elif view == '1': 
+    print("test")
+
+else: 
+    print("new test")
