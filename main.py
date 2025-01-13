43f4b7f7feb97a9bd8f7d8e232c150ac8d4ea8da
secretx09 <freedomfighter3986>
2025-01-13 10:04:37 -0500

    Fixed option 1, added options to option 1

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 view = input('What would you like to see? ("list" for list of options) >>> ')
-if view == "list":
-    print("Activity(1)\nAds and data(2)\nApp Settings(3)\nDM's(4)\nIncome + Wallet Transactions(5)\nPosts(6)\nProfile(7)\nTikTok Live(8)\nTikTok Shopping(9)\n")
+while True:
+    if view == "list":
+        print("Activity(1)\nAds and data(2)\nApp Settings(3)\nDM's(4)\nIncome + Wallet Transactions(5)\nPosts(6)\nProfile(7)\nTikTok Live(8)\nTikTok Shopping(9)\n")
+        
+
+    elif view == '1': 
+        print("Browsing History(1)\nComments(2)\nFavorite effects, hashtags, sounds, videos(3)\nFollowers(4)\nFollowing(5)\nOther(6)")
+
+    elif view =="q":
+        break
+
+    else: 
+        print("new test")
     view = input('What would you like to see? ("list" for list of options) >>> ')
-
-elif view == '1': 
-    print("test")
-
-else: 
-    print("new test")
