9dcce8c0c319b00b7dee8fd750e6f01d408d9c01
secretx09 <freedomfighter3986>
2025-01-13 16:56:40 -0500

    test

--- main.py
+++ main.py
@@ -93,3 +93,6 @@ while True:
     else: 
         print('Please pick a number 1-9')
     view = input('\n\n\n\nWhat would you like to see? ("q to quit") >>> ')
+
+    #test test test 
+    "This is a test"
