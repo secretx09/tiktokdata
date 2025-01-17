c9eb81847cf8c37c13f687e027f248c691a4bb33
secretx09 <freedomfighter3986>
2025-01-17 10:05:59 -0500

    updated every section, made things more clean, added onto option 8

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ rdp = "TikTok_Data_1735588272\Tiktok\Posts\Recently Deleted Posts.txt"
 profile_info = "TikTok_Data_1735588272\Tiktok\Profile\Profile Info.txt"
 ai_moji = "TikTok_Data_1735588272\Tiktok\Profile\AI-Moji.txt"
 auto_info = "TikTok_Data_1735588272\Tiktok\Profile\Autofill.txt"
+live_his = "TikTok_Data_1735588272\Tiktok\TikTok Live\Watch Live History.txt"
+golive_his = "TikTok_Data_1735588272\Tiktok\TikTok Live\Go Live History.txt"
+golive_sets = "TikTok_Data_1735588272\Tiktok\TikTok Live\Go Live settings.txt"
+watchlive_sets = "TikTok_Data_1735588272\Tiktok\TikTok Live\Watch Live settings.txt"
+
+
 
 
 
@@ -39,74 +45,74 @@ while True:
         activity_option = int(input("Browsing History(1)\nComments(2)\nFavorite effects, hashtags, sounds, videos(3)\nFollowers(4)\nFollowing(5)\nShare History(6)\nOther(7)\n>>>"))
         if activity_option == 1:
             with open(browsing_history, "r", encoding='utf-8') as history:
-                browse_his = history.read()
-                print("\n\nBrowsing History: " + browse_his)
+                history = history.read()
+                print("\n\nBrowsing History: " + history)
 
         elif activity_option == 2:
             with open(comments, "r", encoding='utf-8') as comment:
-                comment_his = comment.read()
-                print("\n\nComments: " + comment_his)
+                comment = comment.read()
+                print("\n\nComments: " + comment)
 
         elif activity_option == 3:
             with open(fav_effect, "r", encoding='utf-8') as effect:
-                fav_effects = effect.read()
-                print("\n\nEffects: " + fav_effects)
+                effects = effect.read()
+                print("\n\nEffects: " + effect)
 
             with open(fav_hash, "r", encoding='utf-8') as hash:
-                fav_hashs = hash.read()
-                print("\n\nHashtags: " + fav_hashs)
+                hash = hash.read()
+                print("\n\nHashtags: " + hash)
 
             with open(fav_sound, "r", encoding='utf-8') as sounds:
-                fav_sounds = sounds.read()
-                print("\n\nSounds: " + fav_sounds)
+                sounds = sounds.read()
+                print("\n\nSounds: " + sounds)
 
             with open(fav_video, "r", encoding='utf-8') as videos:
-                fav_videos = videos.read()
-                print("\n\nVideos: " + fav_videos)
+                videos = videos.read()
+                print("\n\nVideos: " + videos)
 
         elif activity_option == 4:
-            with open(followers, "r", encoding='utf-8') as follows:
-                followed_by = follows.read()
+            with open(followers, "r", encoding='utf-8') as followed_by:
+                followed_by = followed_by.read()
                 print("\n\nFollowers: " + followed_by)
 
         elif activity_option == 5:
-            with open(following, "r", encoding='utf-8') as follow:
-                following_acc = follow.read()
-                print("\n\nFollowing: " + following_acc)
+            with open(following, "r", encoding='utf-8') as following:
+                following = following.read()
+                print("\n\nFollowing: " + following)
 
         elif activity_option == 6:
-            with open(share_history, "r", encoding='utf-8') as share:
-                share_his = share.read()
-                print("\n\nShare History: " + share_his)
+            with open(share_history, "r", encoding='utf-8') as shared:
+                shared = shared.read()
+                print("\n\nShare History: " + shared)
 
         elif activity_option == 7:
             with open(hashtags, "r", encoding='utf-8') as hashs:
-                hashtag = hashs.read()
-                print("\n\nHashtags: " + hashtag)
+                hashs = hashs.read()
+                print("\n\nHashtags: " + hashs)
 
             with open(like_list, "r", encoding='utf-8') as like_lists:
-                l_l = like_lists.read()
-                print("\n\nLike list: " + l_l)
+                like_lists = like_lists.read()
+                print("\n\nLike list: " + like_lists)
 
             with open(loc_review, "r", encoding='utf-8') as location:
-                loc_rev = location.read()
-                print("\n\nLocation Reviews?: " + loc_rev)
+                location = location.read()
+                print("\n\nLocation Reviews?: " + location)
 
             with open(login_history, "r", encoding='utf-8') as logins:
-                log_his = logins.read()
-                print("\n\nLogin History: " + log_his)
+                logins = logins.read()
+                print("\n\nLogin History: " + logins)
 
-            with open(purchases, "r", encoding='utf-8') as purchase:
-                purchased = purchase.read()
+            with open(purchases, "r", encoding='utf-8') as purchased:
+                purchased = purchased.read()
                 print("\n\nPurchases: " + purchased)
 
             with open(searches, "r", encoding='utf-8') as search:
-                searched = search.read()
-                print("\n\nSearches: " + searched)
+                search = search.read()
+                print("\n\nSearches: " + search)
 
             with open(status, "r", encoding='utf-8') as stats:
-                stattus = stats.read()
-                print("\n\nStatus: " + stattus)
+                stats = stats.read()
+                print("\n\nStatus: " + stats)
         else:
             print("Please choose one of the options (1-6)")
 
@@ -136,31 +142,31 @@ while True:
                 acc_sets = account_settings.read()
                 print(acc_sets)
         else: 
-            print("Please choose one of the 2 options")
+            print("\n\nPlease choose one of the 2 options")
     
     #Option 4
     elif view == '4':
         with open(direct_messages, "r", encoding='utf-8') as dms:
                 messages = dms.read()
-                print(messages)
+                print("\n\n" +messages)
 
     #Option 5
     elif view == '5':
         with open(transaction_history, "r", encoding='utf-8') as transactions:
                 trans_his = transactions.read()
-                print(trans_his)
+                print("\n\n" + trans_his)
 
     #Option 6
     elif view == '6':
         posts = int(input('Posts(1)\nRecently deleted posts(2)\n>>>'))
         if posts == 1:
             with open(post, "r", encoding='utf-8') as posts:
-                post_his = posts.read()
-                print(post_his)
+                posts = posts.read()
+                print("\n\n" + posts)
         elif posts == 2: 
             with open(rdp, "r", encoding='utf-8') as recently_deleted:
                 recent_deleted = recently_deleted.read()
-                print(recent_deleted)
+                print("\n\n" + recent_deleted)
         else: 
             print("Please choose one of the 2 options")
 
@@ -169,12 +175,13 @@ while True:
         profile = int(input('Profile info(1)\nOther(2)\n>>>'))
         if profile == 1: 
             with open(profile_info, "r", encoding='utf-8') as prof_info:
-                print(prof_info.read())
+                print("\n\n" + prof_info.read())
         elif profile == 2: 
             with open(ai_moji, "r", encoding='utf-8') as ai_emoji:
-                print(ai_emoji.read())
+                print("\n\n" + ai_emoji.read())
+
             with open(auto_info, "r", encoding='utf-8') as auto_fill:
-                print(auto_fill.read())
+                print("\n\n" + auto_fill.read())
         else: 
             print("Please choosse one of the 2 options")
 
@@ -182,13 +189,17 @@ while True:
     elif view == '8':
         live = int(input('Live history *comments, watched lives, etc*(1)\nOther(2)\n>>>'))
         if live == 1: 
-            with open("", "r", encoding='utf-8') as file:
-                content = file.read()
-                print(content)
+            with open(live_his, "r", encoding='utf-8') as live_history:
+                print("\n\n" + live_history.read())
         elif live == 2: 
-            with open("", "r", encoding='utf-8') as file:
-                content = file.read()
-                print(content)
+            with open(golive_his, "r", encoding='utf-8') as golive_history:
+                print("\n\n" + golive_history.read())
+
+            with open(golive_sets, "r", encoding='utf-8') as golive_settings:
+                print("\n\n" + golive_settings.read())
+
+            with open(watchlive_sets, "r", encoding='utf-8') as watchlive_settings:
+                print("\n\n" + watchlive_settings.read())
         else: 
             print("Please choose 1 of the 2 options")
 
